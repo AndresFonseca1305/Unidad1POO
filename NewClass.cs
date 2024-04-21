1fec86687f342bb1e3b04bcfd6bbb0133966b6d5
Andres Fonseca <andresitofonseca13@gmail.com>
2024-04-21 17:09:04 -0500

    finish the exercise in C#

--- NewClass.cs
+++ NewClass.cs
@@ -2,12 +2,12 @@ namespace Actividad1;
 
 public class NewClass
 {
-    public List<string> Names()
+    public static List<string> Names()
     {
         Console.WriteLine("Este metodo solicita los nombres de los alumnos asi que primero se debe ingresar la cantidad de alumnos");
         Console.WriteLine("Cuantos alumnos son?");
         int cantEstudents = int.Parse(Console.ReadLine());
-        List<string> names= new List<string>();
+        List<string> names = new List<string>();
 
         for (int i = 0; i < cantEstudents; i++)
         {
@@ -16,5 +16,26 @@ public class NewClass
         }
 
         return names;
+    }
+    
+    public static Dictionary<string, double> Grades(List<string> names) 
+    {
+        Dictionary<string, double> grades = new Dictionary<string, double>();
+        foreach (var name in names)
+        {
+            double grade;
+            do
+            {
+                Console.WriteLine("Escribe la nota del estudiante, " + name);
+                grade = double.Parse(Console.ReadLine());
+                if (grade < 1.0 || grade > 10.0)
+                {
+                    Console.WriteLine("La nota debe ser entre 1.0 y 10.0");
+                }
+            } while (grade < 1.0 || grade > 10.0);
+            grades.Add(name, grade);
+        }
+
+        return grades;
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Este programa solicita los nombres de los alumnos y sus calificaciones y muestra el promedio del curso y los estudiante con calificaciones menores al promedio");
+            List<string> names = NewClass.Names();
+            Dictionary<string, double> grades = NewClass.Grades(names);
+
+            double average = grades.Values.Average();
+            Console.WriteLine("El promedio del curso es: " + average);
             
+            Console.WriteLine("Estos son los estudiantes con notas mas bajas que el promedio");
+            foreach (var grade in grades)
+            {
+                if (grade.Value < average)
+                {
+                    Console.WriteLine(grade.Key + " con nota: " + grade.Value);
+                }
+            }
         }
     }
 }
